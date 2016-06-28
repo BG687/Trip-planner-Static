81b838b52d13d5866ae3a985a31ac56aef2ce244
ishdaddy7 <seanhan1@gmail.com>
2016-06-27 22:13:01 -0400

    small tweak for getting all data to tee up tomorrow

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,23 @@
 var express = require('express');
 var router = express.Router();
 var models = require('../models');
-
+var Promise = require('bluebird');
+var activities = models.Activity;
+var hotels = models.Hotel;
+var places = models.Place;
+var restaurants  = models.Restaurant;
 
 
 router.get('/', function(req, res, next){
-  models.Place.findAll({})
+/*  models.Place.findAll({})
+  .then(function(result){
+    res.send(result);
+  })*/
+  Promise.all([activities.findAll({}), hotels.findAll({}), places.findAll({}), restaurants.findAll({})])
   .then(function(result){
     res.send(result);
   })
-  //res.send('hi');
-
+  .catch(console.error);
 
   //res.render('index', {});
 });
